8939d4a5367711eda3abf4da566d2cecad7e6954
renatogomesf <renato_gfe@hotmail.com>
2025-05-13 13:56:51 -0300

    finalizado estruturas condicionais

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -48,6 +48,39 @@ public class Aula05EstruturasCondicionais {
         double salario = 3000;
         String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda não tenho condições, mas vou ter!";
 
-        System.out.println(resultado);
+        //System.out.println(resultado);
+
+
+        // SWITCH
+        // imprima o dia da semana, considerando 1 como domingo.
+
+        byte dia = 10;
+
+        switch (dia) {
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda-feira");
+                break;
+            case 3:
+                System.out.println("terça-feira");
+                break;
+            case 4:
+                System.out.println("quarta-feira");
+                break;
+            case 5:
+                System.out.println("quinta-feira");
+                break;
+            case 6:
+                System.out.println("sexta-feira");
+                break;
+            case 7:
+                System.out.println("sábado");
+                break;
+            default:
+                System.out.println("dia incorreto");
+                break;
+        }
     }
 }
