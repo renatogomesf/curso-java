788ebc4a1656cad980881ecaf3b4f29a5cb06ba9
renatogomesf <renato_gfe@hotmail.com>
2025-05-14 14:54:30 -0300

    finalizando array multidimensionais

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
@@ -24,10 +24,32 @@ public class Aula08ArraysMultidimensionais {
 
         // COM FOREACH ================================================================
         // o for de fora faz referencia a cada array. pega array por array e joga em arrBase
-        for (int[] arrBase: dias){
+        for (int[] arrBase : dias) {
             // o for de dentro pega item por item do arrBase da vez e joga em dia.
-            for (int dia: arrBase){
-                System.out.println(dia);
+            for (int dia : arrBase) {
+                //System.out.println(dia);
+            }
+        }
+
+
+        // INICIALIZANDO ARRAYS MULTIDIMENSIONAIS ====================================
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[]{1, 2}; // no indice 0 tem um array de tamanho 2
+        arrayInt[1] = new int[]{1, 2, 3}; // no indice 1 tem um array de tamanho 3
+        arrayInt[2] = new int[]{1, 2, 3, 4, 5, 6}; // no indice 2 tem um array de tamanho 6
+
+        for (int[] arrBase : arrayInt) {
+            for (int num : arrBase) {
+                System.out.println("inicialização 1: " + num);
+            }
+        }
+
+        int[][] arrayInt2 = {{1, 2}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
+
+        for (int[] arrBase : arrayInt2) {
+            for (int num : arrBase) {
+                System.out.println("inicialização 2: " + num);
             }
         }
     }
