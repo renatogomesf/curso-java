d42fa86eda2238c16469fb284211b42fb2c1bba3
renatogomesf <renato_gfe@hotmail.com>
2025-05-14 15:31:29 -0300

    orientação a objetos

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/domain/Estudante.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.domain;
+
+// classes são usadas para representar algo do mundo real. ela pode conter atributos e métodos.
+// elas também podem ter o "public static void main".
+// classes que dão origem a objetos são chamadas de "domain(domínio)/model". representa o mapeamento de algo no mundo real.
+// uma forma que da origem a varios objetos.
+//PASTAS DE DOMAINS contem as regras de negócios que vão, geralmente, para o banco de dados.
+public class Estudante {
+    // atributos/características
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/teste/EstudanteTeste01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.domain.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        // é uma variavel de referência "estudante" do tipo Estudante (classe)
+        // e para criar objetos é preciso usar o new "nome da classe"()
+        // ou seja, a variável que vai criar os objetos dever tipada e instanciada com o nome da classe do objeto que deseja criar.
+        Estudante estudante = new Estudante();
+        estudante.nome = "teste";
+        estudante.idade = 21;
+        estudante.sexo = 'm';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
