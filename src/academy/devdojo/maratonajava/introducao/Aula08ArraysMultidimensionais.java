c47f7b5b27dda6b98d04866bb2ffd3d6cc27cca6
renatogomesf <renato_gfe@hotmail.com>
2025-05-14 10:28:24 -0300

    array multidimensionais parte 02

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula08ArraysMultidimensionais {
     public static void main(String[] args) {
-        //arrays de arrays. arrays linkados.
+        //arrays de arrays. arrays linkados. ============================================
 
         // new int[3][3]: o primeiro colchete diz a quantos arrays ele está linkando/fazendo referência (neste caso, 3 arrays. e para acessar usa índice normalmente) e o segundo diz o tamanho desses arrays (para acessar usa índice normalmente)
         int[][] dias = new int[3][3];
@@ -18,7 +18,16 @@ public class Aula08ArraysMultidimensionais {
 
         for (int i = 0; i < dias.length; i++) {
             for (int j = 0; j < dias[i].length; j++) {
-                System.out.println(dias[i][j]);
+                //System.out.println(dias[i][j]);
+            }
+        }
+
+        // COM FOREACH ================================================================
+        // o for de fora faz referencia a cada array. pega array por array e joga em arrBase
+        for (int[] arrBase: dias){
+            // o for de dentro pega item por item do arrBase da vez e joga em dia.
+            for (int dia: arrBase){
+                System.out.println(dia);
             }
         }
     }
