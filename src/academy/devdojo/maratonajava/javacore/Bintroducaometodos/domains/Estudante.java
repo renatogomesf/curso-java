17d8db698eee91ae5599319d7b4c177a9643db58
renatogomesf <renato_gfe@hotmail.com>
2025-05-16 10:00:19 -0300

    métodos aula 51

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domains/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domains/Estudante.java
@@ -4,4 +4,12 @@ public class Estudante {
     public String nome;
     public int idade;
     public char sexo;
+
+    // métod dentro da classe.
+    // para usar uma variável dentro da classe, usa-se o "this". ele faz referencia às variáveis dentro da classe.
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/teste/EstudanteTeste02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.teste;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domains.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domains.ImprimeEstudante;
+
+public class EstudanteTeste02 {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante1.nome = "Midoriya";
+        estudante1.idade = 15;
+        estudante1.sexo = 'm';
+
+        estudante2.nome = "Sakura";
+        estudante2.idade = 20;
+        estudante2.sexo = 'f';
+
+        estudante1.imprime();
+        estudante2.imprime();
+    }
+}
