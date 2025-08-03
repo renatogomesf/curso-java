773f7422ba38fa1102726c84cb357df15db062fc
renatogomesf <renato_gfe@hotmail.com>
2025-08-03 09:41:41 -0300

    aula 95 exceções.

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/teste/StackOverflowTeste01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Oexception.teste;
+
+public class StackOverflowTeste01 {
+    public static void main(String[] args) {
+
+    }
+
+    // erros: são situações que não tem como recuprar. ex: chamadas excessivas que acabam consumindo toda a memória disponível.
+    // para resolver o problema, precisa parar o programa resolver o problema (simplesmente arrumar o código) e depois reiniciar. e quando se tem erros, normalmente a JVM para de executar.
+//    public static void recursividade() {
+//        recursividade();
+//    }
+}
