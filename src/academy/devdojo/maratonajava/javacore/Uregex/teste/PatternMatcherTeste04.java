8e1ef245b2f4a26b1620d128f826377f6010087b
renatogomesf <renato_gfe@hotmail.com>
2025-10-01 11:21:56 -0300

    aula 134 classes utilitárias.

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/teste/PatternMatcherTeste04.java
@@ -0,0 +1,52 @@
+package academy.devdojo.maratonajava.javacore.Uregex.teste;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTeste04 {
+    public static void main(String[] args) {
+        // usada para fazer buscas/verificações em strings.
+
+        // META CARACTERES
+
+        // \d = RETORNA Todos os dígitos
+        // \D = RETORNA Tudo o que não for dígito
+        // \s = RETORNA Todos os espaços em branco (\t,\n,\f,\r: representam espaços em branco)
+        // \S = RETORNA Todos os caracteres ignorando os espaços em branco
+        // \w = RETORNA Tudo que for de a-z/A-Z, dígitos e "_" (underscore)
+        // \W = RETORNA Tudo que NÃO for incluso no "\w" (a-z/A-Z, dígitos e "_"). ou seja, retorna caracteres especiais e espaços em branco.
+        // [] "Renge" = busca os caracteres específicos colocados dentro do colchetes. ex: [abc], busca por "a", por "b" ou por "c" minúsculo. como é uma busca específica, se existir essas letras em maiúsculo, ele não retornará. ex2: [a-zA-C]: busca por todas as letras minúsculas de a-z e todas as letras maúsculas de A-C.
+        // QUANTIFICADORES são caracteres que vão me permitir pegar uma expreção e refazer ela de acordo com a quantidade que cada meta-caracter representa
+        // ? = Zero ou uma ocorrência
+        // * = Zero ou mais ocorrência
+        // + = uma ou mais ocorrência
+        // {n,m} = de n até m. define um range de ocorrências específicas. ex: 5 a 10...
+        // () = agrupamento
+        // | = "ou" ex: o(v|c)o -> vai dar metch em ovo e oco.
+        // $ = representa o fim da linha/ fim do texto.
+
+        String regex2 = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto2 = "12 0x 0X 0xFFABC 0x10G 0x1";
+
+        Pattern pattern = Pattern.compile(regex2);
+        Matcher matcher = pattern.matcher(texto2);
+
+        System.out.println("texto:   " + texto2);
+        System.out.println("indice:  0123456789");
+        System.out.println("regex:  " + regex2);
+        System.out.println("posições encontradas");
+
+        // find(): faz a busca. caso exista, retorna true.
+        while (matcher.find()){
+
+            // start(): retorna o índice do primeiro elemento do padrão passado. onde o padrão começa em cada parte do texto
+            // group(): retorna o padrão em si encontrado no texto analisado.
+            System.out.print("índice: "+matcher.start()+" | item: "+matcher.group()+"\n");
+        }
+
+        // para esrever hexadecimal diretamente no código java, precisa adicionar "0x" ou "0X" no início do código hex.
+        int numeroHex = 0x59F86A;
+        // mesmo passando o valor hex, ele retorna o valor decimal.
+        System.out.println(numeroHex);
+    }
+}
