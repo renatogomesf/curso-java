0ed103a15d8031521b726b539c972387d2029bfd
renatogomesf <renato_gfe@hotmail.com>
2025-10-14 11:02:32 -0300

    aula 162 coleções

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/domains/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/domains/Smartphone.java
@@ -1,5 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.domains;
 
+import java.awt.image.SampleModel;
+
 public class Smartphone {
     private String serialNumber;
     private String marca;
@@ -7,6 +9,23 @@ public class Smartphone {
     public Smartphone(String serialNumber, String marca) {
         this.serialNumber = serialNumber;
         this.marca = marca;
+    }
+
+    // -----Regras da sobrescrita do metodo "equals()"-----
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simétrico: para x e y diferentes de null, se x.equals(y) == true, logo y.equals(x) == true
+    // Transitividade: ára x, y, z diferentes de null, se x.equals(y) == true e x.equals(z) == true, logo y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem que retornar false
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
     }
 
     public String getSerialNumber() {
--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/EqualsTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/EqualsTeste01.java
@@ -7,7 +7,8 @@ public class EqualsTeste01 {
         Smartphone s1 = new Smartphone("1ABC1","iPhone");
         Smartphone s2 = new Smartphone("1ABC1","iPhone");
 
-        // equals(): verifica se as variáveis fazem referência para o mesmo objeto. não comparam o valor me si.
+        // equals() de um objeto: verifica se as variáveis fazem referência para o mesmo objeto. não comparam o valor me si.
+        // equals() partindo de uma string comparam o valor em si.
         System.out.println(s1.equals(s2));
     }
 }
