edbaa5fd6fe978ee6a4882c435cff764cecd5f10
renatogomesf <renato_gfe@hotmail.com>
2025-10-22 09:03:15 -0300

    aula 171 coleções

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/MangaSortTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/teste/MangaSortTeste01.java
@@ -4,7 +4,19 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.domains.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+
+// quando se implementa o "Comparable" e seu metodo "compareTo", ficar mudando a lógica criada de comparação vai causar problemas. não é uma boa prática. por isso, cria-se um classse/objeto implementando o "Comparator" e o metodo "compare", para fazer outros tipos de comparaçoes desejadas e não mudar o código original.
+class MangaByIdComparator implements Comparator<Manga>{
+
+    // aqui estou definindo que a comparação será por id.
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId());
+    }
+}
 
 public class MangaSortTeste01 {
     public static void main(String[] args) {
@@ -25,6 +37,18 @@ public class MangaSortTeste01 {
         // só é possível usar o "sort()" com essa classe porque a interface "Comparable" e seu metodo "compareTo" foram implementados. eles são chamados autaticamente pelo java e a organização será como eu defini no metodo.
         Collections.sort(mangas);
 
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+
+        System.out.println("----------------------------------------");
+
+        // como "sort()" é sobrecarregado, ele aceita a instanciação de um objeto que implemente o "Comparator" para fazer outro tipo de comparação.
+        Collections.sort(mangas, new MangaByIdComparator());
+
+        // chamando "sort()" direto do objeto, caso eu deseje organizar de outra forma, é poreciso passar o objeto que implementa o "Comparator" com o metodo "compare".
+        mangas.sort(new MangaByIdComparator());
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
